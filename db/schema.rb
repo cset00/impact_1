199ed121bff599a0196fd34cd00a62951ebe0101
Caroline <vc.setiawan@gmail.com>
2018-10-13 10:54:43 +1100

    admin_users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_12_235345) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admin_users", force: :cascade do |t|
+    t.string "firstname"
+    t.string "lastname"
+    t.boolean "active"
+    t.string "email"
+    t.string "pass_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
