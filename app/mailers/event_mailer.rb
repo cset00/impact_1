654baa46814b3271feb23c323c91c5470b985b7a
Leah Eramo <leah.eramo@rea-group.com>
2018-10-13 15:55:00 +1100

    email config

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -2,9 +2,10 @@ class EventMailer < ActionMailer::Base
     default from: 'no-reply@impactforwomen.org.au'
     layout 'mailer'
 
-    def new_event_email
-        @event = params[:event]
-        mail(to: params[:email], subject: 'New upcomming event!')
+    def new_event_email(event, reg_user)
+        @event = event
+        @reg_user = reg_user
+        mail(to: "leah_eramo@hotmail.com", subject: 'New upcomming event!')
     end
 end
 
