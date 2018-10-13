08f81d416892544527764ac322af055a02bf9499
Rachel Ferretto <rachel.ferretto@rea-group.com>
2018-10-13 11:56:19 +1100

    add CRUD methods

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,18 @@
 class EventsController < ApplicationController
 
   def index
+    @events = Events.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def delete
   end
 end
