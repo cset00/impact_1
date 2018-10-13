cabf09b6533995a135eb6cf29058a49c51d6998c
Caroline <vc.setiawan@gmail.com>
2018-10-13 14:38:33 +1100

    admin signup working

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -15,6 +15,20 @@ class AdminUsersController < ApplicationController
   def create
     @admin_user = AdminUser.new
     @admin_user.email = params[:email]
+
+    if @admin_user.save
+      session[:admin_id] = @admin_user.id
+      redirect_to '/events'
+    else
+      render :new
+    end
   end
- end
+
+  private
+  def admin_user_create_params
+      params.require(:admin_user).permit(:email, :password)
+  end
+end
+
+
 
