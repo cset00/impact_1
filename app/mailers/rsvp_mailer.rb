acc62c489877dbe577e4c50100fe71f6456470ef
Leah Eramo <leah.eramo@rea-group.com>
2018-10-13 22:14:47 +1100

    logo added to emails

--- app/mailers/rsvp_mailer.rb
+++ app/mailers/rsvp_mailer.rb
@@ -4,11 +4,13 @@ class RsvpMailer < ActionMailer::Base
 
     def rsvp_email(rsvp)
         @rsvp = rsvp
+        attachments.inline['impact-red-logo.svg'] = File.read(Rails.root.join("public/impact-red-logo.svg"))
         mail(to: @rsvp.reg_user.email, subject: "You're attending '#{@rsvp.event.subject}!")
     end
 
     def cancel_confirm_email(rsvp)
         @rsvp = rsvp
+        attachments.inline['impact-red-logo.svg'] = File.read(Rails.root.join("public/impact-red-logo.svg"))
         mail(to: @rsvp.reg_user.email, subject: "You are no longer marked as attending #{@rsvp.event.subject}!")
     end
 end
