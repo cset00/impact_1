3c013d03f2056eb1242b7ddb64e70ae8324b071a
Tong Yu <rachieyu@gmail.com>
2018-10-13 13:58:49 +1100

    basic mark up of login page for admin

---
+++ app/controllers/admin_users_controller.rb
@@ -0,0 +1,16 @@
+class AdminUsersController < ApplicationController
+  attr_accessor :id, :name
+
+  def index
+		@admin_users = AdminUser.all
+	end
+
+	def show
+		@admin_users = AdminUser.all
+	end
+
+  def new
+      @admin_users = AdminUser.all
+      render :login
+	end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :reg_users, only: [:index, :show, :new]
+  get '/login', to:'admin_users#login'
 
   resources :events
   post '/events/:id/cancel', to: 'events#cancel'
