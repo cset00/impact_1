d0039f690452eb20accc8221286afb0e908c7859
Caroline <vc.setiawan@gmail.com>
2018-10-13 13:49:48 +1100

    commented out session methods in admin controller as it was causing a loop

--- app/controllers/admin_users_controller.rb
+++ app/controllers/admin_users_controller.rb
@@ -9,19 +9,19 @@ class AdminUsersController < ApplicationController
     render :signup
   end
 
-  def session
-    admin = AdminUser.find_by(email: params[:email])
-    if admin && admin.authenticate(params[:passoword])
-        session[:admin_id] = admin.id
-        redirect_to '/events'
-    else
-        render :login
-    end
-  end
+  # def session
+  #   admin = AdminUser.find_by(email: params[:email])
+  #   if admin && admin.authenticate(params[:passoword])
+  #       session[:admin_id] = admin.id
+  #       redirect_to '/events'
+  #   else
+  #       render :login
+  #   end
+  # end
 
-  def delete_session
-    session[:admin_id] = nil
-    redirect_to '/login'
-  end
+  # def delete_session
+  #   session[:admin_id] = nil
+  #   redirect_to '/login'
+  # end
   
 end
