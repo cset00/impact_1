a08bb008bba5887e9cf01fa42e9527bb1a656a8f
Rachel Ferretto <rachel.ferretto@rea-group.com>
2018-10-13 12:51:36 +1100

    create methods for crud

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -5,14 +5,42 @@ class EventsController < ApplicationController
   end
 
   def show
+    @event = Events.find(params[:id])
   end
 
   def new
+    #if logged_in?
+    @event = Event.new
+  end
+
+  def create
+    # redirect if logged_in?
+    @event = Event.new(create_params)
+    @event.save
   end
 
   def edit
+    @event = Event.find(params[:id])
   end
 
-  def delete
+  def update
+    @event = Event.find(params[:id])
+    @event.update_attributes(create_params)
+    @event.save
+    #add redirect
+  end
+
+  def destroy
+    #redirect_to'/' unless logged_in?
+    event = Event.find(params[:id])
+    event.active = false
+    event.save
+    #add redirect
+  end
+
+  private
+  
+  def create_params
+    params.require(:event).permit(:date_timeaddress, :subject, :description)
   end
 end
