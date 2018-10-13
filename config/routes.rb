d6703b3bca9dbfd3247ec2a37344bb8b0d7eea31
Caroline <vc.setiawan@gmail.com>
2018-10-13 14:51:31 +1100

    sorry for working in master. login works too

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
-  get '/login', to: 'admin_users#login'
-
   resources :reg_users, only: [:index, :show, :new, :create]
+  
+  resources :admin_users, only: [:index, :show, :new, :create]
   get '/login', to:'admin_users#login'
-  get '/signup', to:'admin_users#new'
-  post '/create_user', to:'admin_users#create'
+  post '/login', to:'events#index'
 
   resources :events
   post '/events/:id/cancel', to: 'events#cancel'
