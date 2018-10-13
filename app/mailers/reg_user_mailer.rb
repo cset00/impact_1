39eb52a3adb2e350e128229485ac8e2459bf9044
Leah Eramo <leah.eramo@rea-group.com>
2018-10-13 23:00:09 +1100

    email partials for easy styling

--- app/mailers/reg_user_mailer.rb
+++ app/mailers/reg_user_mailer.rb
@@ -1,10 +1,11 @@
-class RegUserMailer < ActionMailer::Base
+class RegUserMailer < ApplicationMailer
     default from: 'no-reply@impactforwomen.org.au'
     layout 'mailer'
 
     def welcome_email(user)
         @reg_user = user
-        attachments.inline['impact-red-logo.svg'] = File.read(Rails.root.join("public/impact-red-logo.svg"))
+        attachments.inline['impact-red-logo.svg'] = logo_att
+        attachments.inline['help.svg'] = help_att
         mail(to: @reg_user.email, subject: 'Thank you for volunteering for Impact!')
     end
 end
